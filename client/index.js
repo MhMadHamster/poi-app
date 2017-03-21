8a0a9ac01edfc75273717ee2ad5239f7bb27fa55
MhMadHamster <mhmad.hamster@gmail.com>
2017-03-22 01:03:55 +0300

    react router

--- client/index.js
+++ client/index.js
@@ -3,11 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const render = Component => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <Router>
+        <Route path="/" component={App} />
+      </Router>
     </AppContainer>,
     document.getElementById('app')
   )

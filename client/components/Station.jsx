ef9fcb4b35f16a9eac2628078c5b227fe30a865d
MhMadHamster <mhmad.hamster@gmail.com>
2017-03-30 19:10:24 +0300

    remove button for stations

--- client/components/Station.jsx
+++ client/components/Station.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-const Station = props => (
-  <div>{props.match.params.stationId}</div>
+const Station = ({ match, history }) => (
+  <div>{match.params.stationId}
+    <button 
+      onClick={() => fetch(`/api/stations/${match.params.stationId}`, 
+      { method: 'DELETE'})
+      .then(() => {
+        history.push('/');
+      })}
+    >
+      Remove
+    </button>
+  </div>
 )
 
-export default Station;
+export default withRouter(Station);
